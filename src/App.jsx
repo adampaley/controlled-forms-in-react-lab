dd34de648e1de1d0963793090503bfa169e7ae3c
Adam Paley <apaley95@gmail.com>
2024-12-19 14:23:54 -0500

    completed exercise 1 of lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+// import
 import { useState } from 'react'
-import './App.css'
+import './App.css'  
+import Bookshelf from './components/Bookshelf/Bookshelf'
 
+// components
 const App = () => {
-  return <h1>Hello, World!</h1>
+
+
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <Bookshelf/>
+    </>
+  )
 }
 
+//exports
 export default App
